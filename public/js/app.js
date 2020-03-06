e0c54efd9377039773ac6af3250fdc842850a4f1
Ritul Malviya <ritulmalviya2612@gmail.com>
2020-03-06 10:57:38 +0530

    Add Google Maps

--- public/js/app.js
+++ public/js/app.js
@@ -86,6 +86,45 @@ function setDay() {
     sat.textContent=day((d.getDay()+5)%7)
     sun.textContent=day((d.getDay()+6)%7)
 }
+window.onload=(e) =>{
+    e.preventDefault()
+    const location='bhopal'
+// messageOne.textContent='Loading...'
+// messageTwo.textContent=''
+fetch('/weather?address='+location).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error)
+        {
+            // messageOne.textContent=data.error
+        alert(data.error)
+        }
+        else{
+            // messageOne.textContent=data.location
+            // messageTwo.textContent=data.forecast
+            setDay();
+            rain.textContent=data.rain
+            date.textContent=d.getDate()+' '+month(d.getMonth())
+            day1.textContent=parseInt(data.temp1)
+            wind1.textContent=parseInt(data.wind)
+            loc.textContent=data.location
+            day2.textContent=parseInt(data.temp2h)
+            day22.textContent=parseInt(data.temp2l)
+            day3.textContent=parseInt(data.temp3h)
+            day33.textContent=parseInt(data.temp3l)
+            day4.textContent=parseInt(data.temp4h)
+            day44.textContent=parseInt(data.temp4l)
+            day5.textContent=parseInt(data.temp5h)
+            day55.textContent=parseInt(data.temp5l)
+            day6.textContent=parseInt(data.temp6h)
+            day66.textContent=parseInt(data.temp6l)
+            day7.textContent=parseInt(data.temp7h)
+            day77.textContent=parseInt(data.temp7l)
+            //console.log(data.location)
+            //console.log(data.forecast)
+        }
+    })
+})
+}
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
 const location=search.value
